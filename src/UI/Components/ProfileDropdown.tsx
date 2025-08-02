031c052cd7cff569c368ed1c2d5bca620ad0571b
AnandNandanK <anandwising@gmail.com>
2025-08-02 18:52:58 +0530

    logout button issue resolve

--- src/UI/Components/ProfileDropdown.tsx
+++ src/UI/Components/ProfileDropdown.tsx
@@ -6,33 +6,38 @@ import { useAppDispatch, useAppSelector } from "../../reducers/hooks";
 import { useNavigate } from "react-router-dom";
 
 export default function ProfileDropdown() {
-  const dispatch=useAppDispatch();
-  const navigate=useNavigate();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const [openMenue, setOpenMenue] = useState(false);
-  const token = useAppSelector((state)=>state.auth.accessToken);
+  const token = useAppSelector((state) => state.auth.accessToken);
 
- function submitHandler(e: React.FormEvent<HTMLFormElement>){
-  e.preventDefault();
-  dispatch(logout(token,navigate,dispatch));
- }
-
+  function handleSubmit() {
+    dispatch(logout(token, navigate, dispatch));
+  }
 
   return (
-    <form onSubmit={submitHandler} >
- <div className="relative">
+    <div className="relative">
       <div
         onClick={() => setOpenMenue(true)}
         className="text-3xl bg-amber-50 rounded-full p-1 hover:cursor-pointer"
       >
         <CgProfile />
       </div>
-        {openMenue && (
-          <div onClick={() => setOpenMenue(false)} className="  absolute left-[-9px] bg-white border rounded-md  text-base">
-            <button type="submit" className="p-1">Logout <span><IoLogOutOutline /></span></button>
+      {openMenue && (
+        
+          <div
+            onClick={handleSubmit}
+            className="  absolute left-[-9px] bg-white border rounded-md  text-base"
+          >
+            <button type="submit" className="p-1">
+              Logout{" "}
+              <span>
+                <IoLogOutOutline />
+              </span>
+            </button>
           </div>
-        )}
+       
+      )}
     </div>
-    </form>
-   
   );
 }
