80c5c82c7352327bb8c04954e7b48897371e6f3d
AnandNandanK <anandwising@gmail.com>
2025-08-26 13:41:23 +0530

    slider working now

--- src/UI/Components/eventsection/Eventspage/EventscardSlider.tsx
+++ src/UI/Components/eventsection/Eventspage/EventscardSlider.tsx
@@ -1,15 +1,16 @@
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import type{ Content } from "../../../../interfaces/eventInterface/eventInterface";
-
+import type { Content } from "../../../../interfaces/eventInterface/eventInterface";
 
 interface EventscardSliderProps {
   events: Content[];
 }
 
-export default function EventscardSlider({ events = [] }: EventscardSliderProps) {
+export default function EventscardSlider({
+  events = [],
+}: EventscardSliderProps) {
   const navigate = useNavigate();
   // const location = useLocation();
 
@@ -51,9 +52,6 @@ export default function EventscardSlider({ events = [] }: EventscardSliderProps)
     ],
   };
 
-    
-
-
   return (
     <div className="py-4">
       <h2 className="text-xl font-bold">You May Also Like</h2>
@@ -62,27 +60,39 @@ export default function EventscardSlider({ events = [] }: EventscardSliderProps)
       <div className="relative lg:ml-6 flex justify-center lg:block">
         <div className="lg:w-3xl md:w-3xl w-80">
           <Slider {...settings}>
-            {events.map((event, index) => (
-              <div
-                key={index}
-                className="p-1 cursor-pointer"
-                onClick={() => navigate(`/events/${event.contentId}`, { state: event })}
-              >
-                <div className="flex flex-col items-center bg-white rounded-lg p-2">
-                  {/* <div className="lg:h-52 lg:w-40 h-40 mt-2"> */}
-                  <div className="h-40 w-24 lg:w-52 lg:h-52 mt-2">
-                    <img
-                      src={event.thumbnail || ""} // fallback in case image key changes
-                      alt={event.title}
-                      className="h-full w-full object-cover rounded-md"
-                    />
+            {events.map((event, index) => {
+              const slug = event.contentName
+                .toLowerCase()
+                .trim()
+                .replace(/&/g, "and") // & ko 'and' me convert
+                .replace(/\s+/g, "-") // spaces ko '-'
+                .replace(/[^\w-]/g, ""); // special characters remove
+
+              return (
+                <div
+                  key={index}
+                  className="p-1 cursor-pointer"
+                  onClick={() =>
+                    navigate(`/events/${slug}/${event.parentCategoryId}`, {
+                      state: event,
+                    })
+                  }
+                >
+                  <div className="flex flex-col items-center bg-white rounded-lg p-2">
+                    <div className="h-40 w-24 lg:w-52 lg:h-52 mt-2">
+                      <img
+                        src={event.thumbnail || ""}
+                        alt={event.title}
+                        className="h-full w-full object-cover rounded-md"
+                      />
+                    </div>
+                    <p className="mt-2 text-center lg:w-40 md:w-30 sm:w-20 text-sm lg:font-semibold lg:text-lg">
+                      {event.title}
+                    </p>
                   </div>
-                  <p className="mt-2 text-center lg:w-40 md:w-30 sm:w-20 text-sm lg:font-semibold lg:text-lg">
-                    {event.title}
-                  </p>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </Slider>
         </div>
       </div>
