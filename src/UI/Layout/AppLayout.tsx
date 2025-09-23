8e2afe254b57f41a237d51f2f07abdc0670fb917
AnandNandanK <anandwising@gmail.com>
2025-09-23 09:50:00 +0530

    using working code

--- src/UI/Layout/AppLayout.tsx
+++ src/UI/Layout/AppLayout.tsx
@@ -1,22 +1,9 @@
-import { useEffect } from "react";
 import Navbar from "../Components/Navbar";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 
 // MainLayout.tsx
 export default function MainLayout() {
-    const navigate = useNavigate();
-
-  const location = useLocation();
-  const rawFrom = location.state?.from;
-  const from = rawFrom ? rawFrom.replace(/^\/ticketing/, "") : "/";
-  console.log("PRINTING FROM...",from);
-
-  useEffect(() => {
-    if (from) {
-      navigate(from, { replace: true, state: {} });
-    }
-  }, [from, navigate]);
   return (
     <>
       <Navbar />
