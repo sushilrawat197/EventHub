b86a2a845254a0db85002246eb4b2533df65b1b1
sushi <sushilkumarrawat83@gmail.com>
2025-10-09 23:59:33 +0530

    Feedback form scroll issue fixed

--- src/UI/Components/common/RateAndReview.tsx
+++ src/UI/Components/common/RateAndReview.tsx
@@ -71,7 +71,7 @@ export default function RateAndReview() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 flex items-center justify-center p-4">
+    <div className="min-h-[calc(100vh-6rem)] bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 flex items-center justify-center p-4">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
