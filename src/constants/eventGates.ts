11639917a4cff21f7b2a94d4eca96fcb98c19b2e
AnandNandanK <anandwising@gmail.com>
2026-05-22 17:18:09 +0530

    new event added for form

--- src/constants/eventGates.ts
+++ src/constants/eventGates.ts
@@ -1,5 +1,5 @@
 // TODO: TEMP SPECIAL EVENT FLOW - remove this file after marathon flow is retired.
-export const SPECIAL_EVENT_ID = 45;
+export const SPECIAL_EVENT_ID = 46;
 
 /** Persists marathon ticket + participant choice across login for special event (individual). */
 export const SPECIAL_MARATHON_REGISTRATION_STASH_KEY = "eventhub:specialMarathonRegistration";
